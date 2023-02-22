3053446d70fb0748fef9ce4f807aedb8f75ef639
Caio Vinicius <viniciuscaioml@gmail.com>
2023-02-22 18:13:18 -0300

    learning react

---
+++ Hello-World/hello-world.js
@@ -0,0 +1,9 @@
+//o que é para ser injetado
+ReactDOM.render(
+  React.createElement(
+    'h1',
+    null,
+    'Hello World'
+  ),
+  document.getElementById('root') // onde é para ser injetado
+)
